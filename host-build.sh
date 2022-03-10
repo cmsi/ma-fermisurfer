80b983aa15296ff91ec03d67cfd9ff4b3c84ed0f
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-03-10 23:42:56 +0900

    update

--- host-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. $(dirname $0)/path.sh
-
-VM="madev10-amd64 madev10-i386 madev9-amd64 madev9-i386 madev8-amd64 madev8-i386 ubuntu18-amd64 ubuntu16-amd64"
-for v in $VM; do
-  cd $HOME/vagrant/$v
-  vagrant ssh -c "sh development/ma-${PACKAGE}/setup.sh"
-  vagrant ssh -c "sh development/ma-${PACKAGE}/build.sh"
-done
